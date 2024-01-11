474a1b4cbea7c430b6bd39b0fd8f7a79cbb7def3
fachri achmad maulana <fachri57@yahoo.com>
2024-01-11 22:46:16 +0700

    minor fixes

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC0XGnh36CoDZjNzePiqaFqiBZ370x3e2I",
-  authDomain: "dad-jokes-c5202.firebaseapp.com",
-  projectId: "dad-jokes-c5202",
-  storageBucket: "dad-jokes-c5202.appspot.com",
-  messagingSenderId: "156872272781",
-  appId: "1:156872272781:web:92baf68f4abe3294812bc4"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
 }
 
 const app = initializeApp(firebaseConfig)
